864423fe25c1113781dcd3303e5143936083d3d7
Václav Ryška <40323016+IT-Sage@users.noreply.github.com>
2020-11-21 16:47:41 +0100

    Implement access to Litecoin API endpoints (#6)

---
+++ src/Tatum/Clients/LitecoinClient.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tatum.Blockchain;
+using Tatum.Model.Requests;
+using Tatum.Model.Responses;
+
+namespace Tatum.Clients
+{
+    public partial class LitecoinClient : ILitecoinClient
+    {
+        private readonly ILitecoinApi litecoinApi;
+
+        public LitecoinClient(string apiBaseUrl, string xApiKey)
+        {
+            litecoinApi = RestClientFactory.Create<ILitecoinApi>(apiBaseUrl, xApiKey);
+        }
+
+        Task<TransactionHash> ILitecoinClient.BroadcastSignedTransaction(BroadcastRequest request)
+        {
+            return litecoinApi.BroadcastSignedTransaction(request);
+        }
+
+        Task<LitecoinBlock> ILitecoinClient.GetBlock(string hash)
+        {
+            return litecoinApi.GetBlock(hash);
+        }
+
+        Task<LitecoinInfo> ILitecoinClient.GetBlockchainInfo()
+        {
+            return litecoinApi.GetBlockchainInfo();
+        }
+
+        Task<BlockHash> ILitecoinClient.GetBlockHash(long blockHeight)
+        {
+            return litecoinApi.GetBlockHash(blockHeight);
+        }
+
+        Task<LitecoinTx> ILitecoinClient.GetTransaction(string hash)
+        {
+            return litecoinApi.GetTransaction(hash);
+        }
+
+        Task<List<LitecoinTx>> ILitecoinClient.GetTxForAccount(string address, int pageSize, int offset)
+        {
+            return litecoinApi.GetTxForAccount(address, pageSize, offset);
+        }
+
+        Task<LitecoinUtxo> ILitecoinClient.GetUtxo(string txHash, int txOutputIndex)
+        {
+            return litecoinApi.GetUtxo(txHash, txOutputIndex);
+        }
+    }
+}
---
+++ src/Tatum/Model/Responses/Litecoin/LitecoinTx.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Tatum.Model.Responses
+{
+    public class LitecoinTx
+    {
+        [JsonPropertyName("hash")]
+        public string Hash { get; set; }
+
+        [JsonPropertyName("witnessHash")]
+        public string WitnessHash { get; set; }
+
+        [JsonPropertyName("fee")]
+        public string Fee { get; set; }
+
+        [JsonPropertyName("rate")]
+        public string Rate { get; set; }
+
+        [JsonPropertyName("ps")]
+        public long Ps { get; set; }
+
+        [JsonPropertyName("height")]
+        public long Height { get; set; }
+
+        [JsonPropertyName("block")]
+        public string Block { get; set; }
+
+        [JsonPropertyName("ts")]
+        public long Time { get; set; }
+
+        [JsonPropertyName("index")]
+        public int Index { get; set; }
+
+        [JsonPropertyName("version")]
+        public long Version { get; set; }
+
+        [JsonPropertyName("flag")]
+        public int Flag { get; set; }
+
+        [JsonPropertyName("inputs")]
+        public List<LitecoinTxInput> Inputs { get; set; }
+
+        [JsonPropertyName("outputs")]
+        public List<LitecoinTxOutput> Outputs { get; set; }
+
+        [JsonPropertyName("locktime")]
+        public long Locktime { get; set; }
+    }
+
+    public class LitecoinTxInput
+    {
+        [JsonPropertyName("prevout")]
+        public LitecoinTxPrevout Prevout { get; set; }
+
+        [JsonPropertyName("script")]
+        public string Script { get; set; }
+
+        [JsonPropertyName("witness")]
+        public string Witness { get; set; }
+
+        [JsonPropertyName("sequence")]
+        public long Sequence { get; set; }
+
+        [JsonPropertyName("coin")]
+        public LitecoinTxCoin Coin { get; set; }
+    }
+
+    public class LitecoinTxOutput
+    {
+        [JsonPropertyName("value")]
+        public string Value { get; set; }
+
+        [JsonPropertyName("script")]
+        public string Script { get; set; }
+
+        [JsonPropertyName("address")]
+        public string Address { get; set; }
+    }
+
+    public class LitecoinTxCoin
+    {
+        [JsonPropertyName("version")]
+        public int Version { get; set; }
+
+        [JsonPropertyName("height")]
+        public long Height { get; set; }
+
+        [JsonPropertyName("value")]
+        public long Value { get; set; }
+
+        [JsonPropertyName("script")]
+        public string Script { get; set; }
+
+        [JsonPropertyName("address")]
+        public string Address { get; set; }
+
+        [JsonPropertyName("coinbase")]
+        public bool Coinbase { get; set; }
+    }
+
+    public class LitecoinTxPrevout
+    {
+        [JsonPropertyName("hash")]
+        public string Hash { get; set; }
+
+        [JsonPropertyName("index")]
+        public long Index { get; set; }
+    }
+}
