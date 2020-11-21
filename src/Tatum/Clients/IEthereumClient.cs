cdddd6ba6a6f330152cff31ee88030087fb818b1
Václav Ryška <40323016+IT-Sage@users.noreply.github.com>
2020-11-21 20:53:37 +0100

    Implement access to Ethereum API enpoints (#7)

---
+++ src/Tatum/Clients/IEthereumClient.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tatum.Model.Requests;
+using Tatum.Model.Responses;
+
+namespace Tatum.Clients
+{
+    public interface IEthereumClient
+    {
+        Task<TransactionHash> BroadcastSignedTransaction(BroadcastRequest request);
+        Task<int> GetTransactionsCount(string address);
+        Task<long> GetCurrentBlock();
+        Task<EthereumBlock> GetBlock(string hash);
+        Task<EthereumAccountBalance> GetAccountBalance(string address);
+        Task<EthereumAccountBalance> GetErc20AccountBalance(string address, string currency, string contractAddress);
+        Task<EthereumTransaction> GetTransaction(string hash);
+        Task<List<EthereumTransaction>> GetAccountTransactions(string address, int pageSize = 50, int offset = 0);
+    }
+}
---
+++ src/Tatum/Model/Responses/Ethereum/EthereumAccountBalance.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Tatum.Model.Responses
+{
+    public class EthereumAccountBalance
+    {
+        [JsonPropertyName("balance")]
+        public string Balance { get; set; }
+    }
+}
