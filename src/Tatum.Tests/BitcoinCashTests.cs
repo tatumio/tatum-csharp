06bdcfb15c35904ea5c51b6ffa2a28c933b105c5
Václav Ryška <ryska.v@gmail.com>
2020-11-23 22:42:50 +0100

    Implement BitcoinCash wallet

--- src/Tatum.Tests/BitcoinCashTests.cs
+++ src/Tatum.Tests/BitcoinCashTests.cs
@@ -9,6 +9,7 @@ namespace Tatum.Tests
     public class BitcoinCashTests
     {
         IBitcoinCashClient bitcoinCashClient;
+        readonly string mnemonic = "quantum tobacco key they maid mean crime youth chief jungle mind design broken tilt bus shoulder leaf good forward erupt split divert bread kitten";
 
         [SetUp]
         public void Setup()
@@ -30,6 +31,62 @@ namespace Tatum.Tests
         {
             var response = await bitcoinCashClient.GetBlockchainInfo();
             Assert.Pass();
+        }
+
+        [Test]
+        public void CreateWalletMainNet()
+        {
+            string expectedXPub = "xpub6EafivSZvqR8ysLKS52NDKfn16sB9uhCEfCKdYi7PpGqqK3fJGdd53DzUnWYvFRZKAC7pB8FVnvuJKkJparfjjfVPTQTmC7dfC6aVvw6f98";
+            Wallet wallet = bitcoinCashClient.CreateWallet(mnemonic, false);
+
+            Assert.That(mnemonic, Is.EqualTo(wallet.Mnemonic));
+            Assert.That(expectedXPub, Is.EqualTo(wallet.XPub));
+        }
+
+        [Test]
+        public void CreateWalletTestNet()
+        {
+            string expectedTPub = "tpubDExJFAGFe7NbFfXAtG1TRF19LDxq9JCFnHncz6mFjj2jabiNNVUiDUtpipbLSkNo74j2Rke82tkwzWEvDShudB7nT49mSimsF9gzFwTf4nw";
+            Wallet wallet = bitcoinCashClient.CreateWallet(mnemonic, true);
+
+            Assert.That(mnemonic, Is.EqualTo(wallet.Mnemonic));
+            Assert.That(expectedTPub, Is.EqualTo(wallet.XPub));
+        }
+
+        [Test]
+        public void GeneratePrivateKeyMainNet()
+        {
+            string expectedPrivateKey = "KzqM77kK7zqZGockuB2Tov1FXoH6BTMaT3ixeqTPXLAYp838W3KT";
+            var privateKey = bitcoinCashClient.GeneratePrivateKey(mnemonic, 1, false);
+
+            Assert.That(expectedPrivateKey, Is.EqualTo(privateKey));
+        }
+
+        [Test]
+        public void GeneratePrivateKeyTestNet()
+        {
+            string expectedPrivateKey = "cRCLa2kAZ4XpSF62HaqbBEWKA2aVquTGX5sRmFuu2SpZ4s72vi5Y";
+            var privateKey = bitcoinCashClient.GeneratePrivateKey(mnemonic, 1, true);
+
+            Assert.That(expectedPrivateKey, Is.EqualTo(privateKey));
+        }
+
+        [Test]
+        public void GenerateAddressMainNet()
+        {
+            string expectedAddress = "bitcoincash:qr9wgjtyjd4q60323gd2ytsv5w3thl92rclzrklply";
+            string address = bitcoinCashClient.GenerateAddress("xpub6EafivSZvqR8ysLKS52NDKfn16sB9uhCEfCKdYi7PpGqqK3fJGdd53DzUnWYvFRZKAC7pB8FVnvuJKkJparfjjfVPTQTmC7dfC6aVvw6f98", 1, false);
+
+            Assert.That(expectedAddress, Is.EqualTo(address));
+        }
+
+        [Test]
+        public void GenerateAddressTestNet()
+        {
+            string expectedAddress = "bchtest:qr9wgjtyjd4q60323gd2ytsv5w3thl92rcms83akcc";
+            string address = bitcoinCashClient.GenerateAddress("tpubDExJFAGFe7NbFfXAtG1TRF19LDxq9JCFnHncz6mFjj2jabiNNVUiDUtpipbLSkNo74j2Rke82tkwzWEvDShudB7nT49mSimsF9gzFwTf4nw", 1, true);
+
+            Assert.That(expectedAddress, Is.EqualTo(address));
         }
     }
 }
