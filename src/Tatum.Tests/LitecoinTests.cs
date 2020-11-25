81401287ea5b62c330831a98d422694c272d409a
Václav Ryška <ryska.v@gmail.com>
2020-11-25 21:04:35 +0100

    Initial commit

--- src/Tatum.Tests/LitecoinTests.cs
+++ src/Tatum.Tests/LitecoinTests.cs
@@ -9,6 +9,7 @@ namespace Tatum.Tests
     public class LitecoinTests
     {
         ILitecoinClient litecoinClient;
+        readonly string mnemonic = "quantum tobacco key they maid mean crime youth chief jungle mind design broken tilt bus shoulder leaf good forward erupt split divert bread kitten";
 
         [SetUp]
         public void Setup()
@@ -30,6 +31,62 @@ namespace Tatum.Tests
         {
             var response = await litecoinClient.GetBlockchainInfo();
             Assert.Pass();
+        }
+
+        [Test]
+        public void CreateWalletMainNet()
+        {
+            string expectedXPub = "Ltub2aXe9g8RPgAcY6jb6FftNJfQXHMV6UNBeZwrWH1K3vjpua9u8uj95xkZyCC4utdEbfYeh9TwxcUiFy2mGzBCJVBwW3ezHmLX2fHxv7HUt8J";
+            Wallet wallet = litecoinClient.CreateWallet(mnemonic, false);
+
+            Assert.That(mnemonic, Is.EqualTo(wallet.Mnemonic));
+            Assert.That(expectedXPub, Is.EqualTo(wallet.XPub));
+        }
+
+        [Test]
+        public void CreateWalletTestNet()
+        {
+            string expectedTPub = "ttub4giastL5S3AicjXRBEJt7uq22b611rJvVfTgJSRfYeyZkwXwKnZcctK3tEjMpqrgiNSnYAzkKPJDxGoKNWQzkzTJxSryHbaYxsYW9Vr6AYQ";
+            Wallet wallet = litecoinClient.CreateWallet(mnemonic, true);
+
+            Assert.That(mnemonic, Is.EqualTo(wallet.Mnemonic));
+            Assert.That(expectedTPub, Is.EqualTo(wallet.XPub));
+        }
+
+        [Test]
+        public void GeneratePrivateKeyMainNet()
+        {
+            string expectedPrivateKey = "KwrYonf8pFfyQR87NTn124Ep9zoJsZMBCoVUi7mjMc1eTHDyLyBN";
+            var privateKey = litecoinClient.GeneratePrivateKey(mnemonic, 1, false);
+
+            Assert.That(expectedPrivateKey, Is.EqualTo(privateKey));
+        }
+
+        [Test]
+        public void GeneratePrivateKeyTestNet()
+        {
+            string expectedPrivateKey = "cQ1YZMep3CiAnMTA9y62ha6BjGaaTFsTvtDuGmucGvpAVmS89khV";
+            var privateKey = litecoinClient.GeneratePrivateKey(mnemonic, 1, true);
+
+            Assert.That(expectedPrivateKey, Is.EqualTo(privateKey));
+        }
+
+        [Test]
+        public void GenerateAddressMainNet()
+        {
+            string expectedAddress = "1HWYaP13JKtaW2Mhq69NVeSLjRYGpD3aKv";
+            string address = litecoinClient.GenerateAddress("xpub6EsCk1uU6cJzqvP9CdsTiJwT2rF748YkPnhv5Qo8q44DG7nn2vbyt48YRsNSUYS44jFCW9gwvD9kLQu9AuqXpTpM1c5hgg9PsuBLdeNncid", 1, false);
+
+            Assert.That(expectedAddress, Is.EqualTo(address));
+        }
+
+        [Test]
+        public void GenerateAddressTestNet()
+        {
+            string expectedAddress = "mjJotvHmzEuyXZJGJXXknS6N3PWQnw6jf5";
+            string address = litecoinClient.GenerateAddress("tpubDFjLw3ykn4aB7fFt96FaqRjSnvtDsU2wpVr8GQk3Eo612LS9jo9JgMkQRfYVG248J3pTBsxGg3PYUXFd7pReNLTeUzxFcUDL3zCvrp3H34a", 1, true);
+
+            Assert.That(expectedAddress, Is.EqualTo(address));
         }
     }
 }
