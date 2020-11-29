071b5872f74ab59b58960cb589d44b5ad71c97d6
Václav Ryška <40323016+IT-Sage@users.noreply.github.com>
2020-11-29 15:59:30 +0100

    Add implementation of BitcoinCash transactions. (#14)

--- src/Tatum.Tests/BitcoinCashTests.cs
+++ src/Tatum.Tests/BitcoinCashTests.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Threading.Tasks;
 using Tatum.Clients;
+using Tatum.Model.Requests;
 
 namespace Tatum.Tests
 {
@@ -87,6 +90,68 @@ namespace Tatum.Tests
             string address = bitcoinCashClient.GenerateAddress("tpubDExJFAGFe7NbFfXAtG1TRF19LDxq9JCFnHncz6mFjj2jabiNNVUiDUtpipbLSkNo74j2Rke82tkwzWEvDShudB7nT49mSimsF9gzFwTf4nw", 1, true);
 
             Assert.That(expectedAddress, Is.EqualTo(address));
+        }
+
+        [Test]
+        [SetCulture("en-US")]
+        public void TransactionFromUtxo()
+        {
+            var body = new TransferBchBlockchain
+            {
+                FromUtxos = new List<FromUtxoBcash>
+                {
+                    new FromUtxoBcash
+                    {
+                        TxHash = "c98d3d32663b7c659e9768b0cd33b2b50843841dfa5c1af6b67a1caeb0c2b218",
+                        Index = 1,
+                        PrivateKey = "cSmnhYYG2mXRPvi1FoFDihT4bL5qy9DDhephoubJ7mwxb2sgLNGQ",
+                        Value = "0.00096"
+                    }
+                },
+                Tos = new List<To>
+                {
+                    new To
+                    {
+                        Address = "bchtest:qq66lcjtj40fvlhmfpkgu85hujsssear4q6g3z03g8",
+                        Value = 0.00001m
+                    },
+                    new To
+                    {
+                        Address = "bchtest:qqp855jnqaguq9jjnxumsvv8gmph88nnqvkamae0pz",
+                        Value = 0.00094m
+                    }
+                }
+            };
+
+            var txData = bitcoinCashClient.PrepareSignedTransaction(body, true);
+            var expectedTxData = "010000000118b2c2b0ae1c7ab6f61a5cfa1d844308b5b233cdb068979e657c3b66323d8dc9010000006a473044022005afb0775ef33ccf0738f34ff024c8a498f8902f84450234ddc8766bd5ddbe86022034227a83416a347c09bfe2b1d6d905e19a17a5ebeb5c3ebc5ef2b7b53aaa8f6d412102de7edfed42e7b8166ffb37451595183435ab46e5fa715d7ffe77da90a238df87ffffffff02e8030000000000001976a91435afe24b955e967efb486c8e1e97e4a10867a3a888ac306f0100000000001976a914027a52530751c0165299b9b8318746c3739e730388ac00000000";
+            Assert.That(expectedTxData, Is.EqualTo(txData));
+        }
+
+        [Test]
+        [SetCulture("en-US")]
+        public void MissingTo()
+        {
+            var body = new TransferBchBlockchain
+            {
+                FromUtxos = new List<FromUtxoBcash>
+                {
+                    new FromUtxoBcash
+                    {
+                        TxHash = "c98d3d32663b7c659e9768b0cd33b2b50843841dfa5c1af6b67a1caeb0c2b218",
+                        Index = 1,
+                        PrivateKey = "cSmnhYYG2mXRPvi1FoFDihT4bL5qy9DDhephoubJ7mwxb2sgLNGQ",
+                        Value = "0.00096"
+                    }
+                }
+            };
+
+            Assert.That(
+                () => bitcoinCashClient.PrepareSignedTransaction(body, true),
+                Throws.TypeOf<ValidationException>()
+                    .With
+                    .Message
+                    .EqualTo("Tos can not be empty."));
         }
     }
 }
--- src/Tatum/Clients/IBitcoinCashClient.cs
+++ src/Tatum/Clients/IBitcoinCashClient.cs
@@ -17,5 +17,9 @@ namespace Tatum.Clients
         Wallet CreateWallet(string mnemonic, bool testnet);
         string GeneratePrivateKey(string mnemonic, int index, bool testnet);
         string GenerateAddress(string xPubString, int index, bool testnet);
+
+        Task<string> SignKmsTransaction(TransactionKms tx, List<string> privateKeys, bool testnet);
+        string PrepareSignedTransaction(TransferBchBlockchain body, bool testnet);
+        Task<TransactionHash> SendTransaction(TransferBchBlockchain body, bool testnet);
     }
 }
