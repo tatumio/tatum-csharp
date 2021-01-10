6e6f7daa0abefb13bdf885b5e11368e12cccce26
Václav Ryška <40323016+IT-Sage@users.noreply.github.com>
2021-01-10 16:40:09 +0100

    Implement Stellar (XLM) wallet. (#19)

--- src/Tatum/Clients/IXlmClient.cs
+++ src/Tatum/Clients/IXlmClient.cs
@@ -15,5 +15,12 @@ namespace Tatum.Clients
         Task<XlmTx> GetLedgerTransaction(string sequence);
         Task<XlmTx> GetTransaction(string hash);
         Task<List<XlmTx>> GetAccountTransactions(string address);
+
+        /// <summary>
+        /// Generate Stellar address and secret
+        /// </summary>
+        /// <param name="secret">secret of the account to generate address</param>
+        /// <returns>Wallet</returns>
+        Wallet CreateWallet(string secret = null);
     }
 }
