261a023a748d20b74a431fa10e8da993ad9c72fd
Ayuba Muazu <35365191+yubzee@users.noreply.github.com>
2021-10-01 14:37:52 +0100

    Add files via upload

---
+++ Tatum/Model/Kms/SecurityTransactions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Transactions
+/// </summary>
+
+public class SecurityTransactions
+{
+    public string id { get; set; }
+    public string chain { get; set; }
+    public string[] hashes { get; set; }
+    public string serializedTransaction { get; set; }
+    public string withdrawalId { get; set; }
+    public int index { get; set; }
+    public string txId { get; set; }
+    public string status { get; set; }
+    public Withdrawalrespons[] withdrawalResponses { get; set; }
+}
+
+public class Withdrawalrespons
+{
+}
