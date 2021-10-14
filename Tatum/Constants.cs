a8bb1c56602df80af5ce276a8d90f707cfa95e2c
Ayuba Muazu <35365191+yubzee@users.noreply.github.com>
2021-10-14 03:42:51 -1200

    Add files via upload

---
+++ Tatum/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace Tatum
+{
+    public class Constants
+    {
+        public const string BtcKeyDerivationPath = "m/44\'/0\'/0\'/0";
+        public const string LtcKeyDerivationPath = "m/44\'/2\'/0\'/0";
+        public const string BchKeyDerivationPath = "m/44\'/145\'/0\'/0";
+        public const string EthKeyDerivationPath = "m/44\'/60\'/0\'/0";
+        public const string VetKeyDerivationPath = "m/44\'/818\'/0\'/0";
+        public const string TestKeyDerivationPath = "m/44\'/1\'/0\'/0";
+        public const string QtumKeyDerivationPath = "m'/44'/2301'/0'/0";
+        public const string DogeKeyDerivationPath = "m'/44'/3'/0'/0";
+    }
+}
