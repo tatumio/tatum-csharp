c598d358944629a2fe47c1fc3034e63a32c04f98
Ayuba Muazu <35365191+yubzee@users.noreply.github.com>
2021-10-14 03:30:56 -1200

    Add files via upload

--- Tatum/Model/Responses/TransactionHash.cs
+++ Tatum/Model/Responses/TransactionHash.cs
@@ -1,1 +1,16 @@
-sip
+﻿using Newtonsoft.Json;
+
+namespace Tatum.Model.Responses
+{
+    public class TransactionHash
+    {
+        /// <summary>
+        /// TX hash of successful transaction.
+        /// </summary>
+        [JsonProperty("txId")]
+        public string TxId { get; set; }
+
+        [JsonProperty("failed")]
+        public bool Failed { get; set; }
+    }
+}
