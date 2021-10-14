c520fe7afac8b5961142bb7a83275a9562f34fa9
Ayuba Muazu <35365191+yubzee@users.noreply.github.com>
2021-10-14 03:23:51 -1200

    Add files via upload

---
+++ Tatum/Model/Requests/BroadcastRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Tatum.Model.Requests
+{
+    public class BroadcastRequest
+    {
+        [Required]
+        [StringLength(500000, MinimumLength = 1)]
+        [JsonProperty("txData")]
+        public string TxData { get; set; }
+
+        [StringLength(36, MinimumLength = 36)]
+        [JsonProperty("signatureId")]
+        public string SignatureId { get; set; }
+    }
+}
