044eaddb3a372abc13df77a33685f9c84a3b549b
Ayuba Muazu <35365191+yubzee@users.noreply.github.com>
2021-10-14 03:29:44 -1200

    Add files via upload

--- Tatum/Model/Responses/Litecoin/LitecoinBlock.cs
+++ Tatum/Model/Responses/Litecoin/LitecoinBlock.cs
@@ -1,1 +1,35 @@
-fdf
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Tatum.Model.Responses
+{
+    public class LitecoinBlock
+    {
+        [JsonProperty("hash")]
+        public string Hash { get; set; }
+
+        [JsonProperty("height")]
+        public long Height { get; set; }
+
+        [JsonProperty("version")]
+        public long Version { get; set; }
+
+        [JsonProperty("prevBlock")]
+        public string PreviousBlockHash { get; set; }
+
+        [JsonProperty("merkleRoot")]
+        public string MerkleRoot { get; set; }
+
+        [JsonProperty("ts")]
+        public long Time { get; set; }
+
+        [JsonProperty("bits")]
+        public long Bits { get; set; }
+
+        [JsonProperty("nonce")]
+        public long Nonce { get; set; }
+
+        [JsonProperty("txs")]
+        public List<LitecoinTx> Txs { get; set; }
+    }
+}
---
+++ Tatum/Model/Responses/Litecoin/LitecoinInfo.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Tatum.Model.Responses
+{
+    public class LitecoinInfo
+    {
+        [JsonProperty("chain")]
+        public string Chain { get; set; }
+
+        [JsonProperty("blocks")]
+        public long Blocks { get; set; }
+
+        [JsonProperty("headers")]
+        public long Headers { get; set; }
+
+        [JsonProperty("bestblockhash")]
+        public string BestBlockHash { get; set; }
+
+        [JsonProperty("difficulty")]
+        public double Difficulty { get; set; }
+    }
+}
