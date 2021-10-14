deb4ba5e3241944aa8c0e13b2ee92618bf44e2a5
Ayuba Muazu <35365191+yubzee@users.noreply.github.com>
2021-10-14 03:22:14 -1200

    Add files via upload

--- Tatum/Model/Requests/BitcoinCash/TransferBchBlockchain.cs
+++ Tatum/Model/Requests/BitcoinCash/TransferBchBlockchain.cs
@@ -1,1 +1,51 @@
-dfd
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tatum.Model.Requests
+{
+    public class TransferBchBlockchain : IValidatableObject
+    {
+        public List<FromUtxoBcash> FromUtxos { get; set; }
+        public List<Tox> Tos { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (FromUtxos != null && FromUtxos.Count > 0)
+            {
+                foreach (FromUtxoBcash utxo in FromUtxos)
+                {
+                    if (!double.TryParse(utxo.Value, out _))
+                    {
+                        yield return new ValidationResult("Value must be a number.");
+                    }
+
+                    var utxoContext = new ValidationContext(utxo);
+                    Validator.ValidateObject(utxo, utxoContext, validateAllProperties: true);
+                }
+            }
+            else
+            {
+                yield return new ValidationResult("FromUtxos can not be empty.");
+            }
+
+            if (Tos != null && Tos.Count > 0)
+            {
+                foreach (Tox to in Tos)
+                {
+                    var toContext = new ValidationContext(to);
+                    Validator.ValidateObject(to, toContext, validateAllProperties: true);
+                }
+            }
+            else
+            {
+                yield return new ValidationResult("Tos can not be empty.");
+            }
+        }
+    }
+
+    public class FromUtxoBcash : FromUtxo
+    {
+        [Required]
+        public string Value { get; set; }
+    }
+}
