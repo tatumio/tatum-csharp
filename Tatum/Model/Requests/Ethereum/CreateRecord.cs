14443f1ded45c3ae5684898191530e799756be61
Ayuba Muazu <35365191+yubzee@users.noreply.github.com>
2021-10-17 03:02:27 -1200

    Add files via upload

--- Tatum/Model/Requests/Ethereum/CreateRecord.cs
+++ Tatum/Model/Requests/Ethereum/CreateRecord.cs
@@ -1,1 +1,42 @@
-fdfd
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tatum.Model.Requests
+{
+    public class CreateRecord : IValidatableObject
+    {
+        [Required]
+        [StringLength(130000, MinimumLength = 1)]
+        public string Data { get; set; }
+
+        [Required]
+        [StringLength(66, MinimumLength = 32)]
+        public string FromPrivatekey { get; set; }
+
+        [StringLength(42, MinimumLength = 42)]
+        public string To { get; set; }
+
+        [Range(0, uint.MaxValue)]
+        public uint Nonce { get; set; }
+
+        public EthFee EthFee { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> results = new List<ValidationResult>();
+
+            if (EthFee != null)
+            {
+                var feeContext = new ValidationContext(EthFee);
+                Validator.ValidateObject(EthFee, feeContext, validateAllProperties: true);
+            }
+
+            if (results.Count == 0)
+            {
+                results.Add(ValidationResult.Success);
+            }
+
+            return results;
+        }
+    }
+}
---
+++ Tatum/Model/Requests/Ethereum/EthFee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace Tatum.Model.Requests
+{
+    public class EthFee
+    {
+        [Required]
+        public double GasLimit { get; set; }
+
+        [Required]
+        public BigInteger GasPrice { get; set; }
+    }
+}
