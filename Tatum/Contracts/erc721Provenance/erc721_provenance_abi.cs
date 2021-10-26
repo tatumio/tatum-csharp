8a074c46df7291a77f6afffbeb0b8ed78fc38f3d
Ayuba Muazu <35365191+yubzee@users.noreply.github.com>
2021-10-26 08:08:30 -1200

    Add files via upload

---
+++ Tatum/Contracts/erc721Provenance/erc721_provenance_abi.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tatum.Contracts
+{
+    public class erc721_provenance_abi
+    {
+        public string _erc721provenanceabi = "";
+
+    }
+}
