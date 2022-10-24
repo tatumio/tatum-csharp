e0487ca3fa14161149130c4b37528dc683921eea
Smrecz <69101062+Smrecz@users.noreply.github.com>
2022-10-24 12:31:46 +0200

    Ethereum - add Examples (#35)

---
+++ Examples/Ethereum/ETH_Examples.md
@@ -0,0 +1,56 @@
+# How to use Tatum.CSharp.Ethereum with .NET 6 API project
+
+## How to start
+
+Add Tatum.CSharp.Ethereum to your project
+
+```bash
+dotnet add package Tatum.CSharp.Ethereum
+```
+
+Register EthereumClient in the DI Container
+
+```csharp
+builder.Services
+    .AddHttpClient<IEthereumClient, EthereumClient>(httpClient => new EthereumClient(httpClient, apiKey));
+```
+
+Inject IEthereumClient into your service
+
+```csharp
+public class EthereumController : ControllerBase
+{
+    private readonly IEthereumClient _ethereumClient;
+
+    public EthereumController(IEthereumClient ethereumClient)
+    {
+        _ethereumClient = ethereumClient;
+    }
+}
+```
+
+### How to generate Ethereum wallet
+
+Check example [here](https://github.com/tatumio/tatum-csharp/blob/master/Tatum.CSharp.Demo/Controllers/EthereumController.cs#L21)
+
+### How to generate Ethereum private key from mnemonic
+
+Check example [here](https://github.com/tatumio/tatum-csharp/blob/master/Tatum.CSharp.Demo/Controllers/EthereumController.cs#L32)
+
+
+### How to generate Ethereum address from private key
+
+Check example [here](https://github.com/tatumio/tatum-csharp/blob/master/Tatum.CSharp.Demo/Controllers/EthereumController.cs#L43)
+
+### How to get balance of Ethereum address
+
+Check example [here](https://github.com/tatumio/tatum-csharp/blob/master/Tatum.CSharp.Demo/Controllers/EthereumController.cs#L51)
+
+### How to get Ethereum transaction by hash
+
+Check example [here](https://github.com/tatumio/tatum-csharp/blob/master/Tatum.CSharp.Demo/Controllers/EthereumController.cs#L59)
+
+### How to send Ethereum from account to account
+
+Check example [here](https://github.com/tatumio/tatum-csharp/blob/master/Tatum.CSharp.Demo/Controllers/EthereumController.cs#L82)
+
--- Tatum.CSharp.Demo/Controllers/EthereumController.cs
+++ Tatum.CSharp.Demo/Controllers/EthereumController.cs
@@ -16,9 +16,71 @@ public class EthereumController : ControllerBase
     }
 
     [HttpGet(Name = "GenerateWallet")]
-    public async Task<Wallet> GenerateWallet()
+    public Wallet GenerateWallet()
     {
+        Wallet wallet = _ethereumClient.Local.GenerateWallet();
         
-        return await _ethereumClient.EthereumBlockchain.EthGenerateWalletAsync();
+        return wallet;
+    }
+    
+    [HttpPost(Name = "GeneratePrivateKey")]
+    public PrivKey GeneratePrivateKey([FromBody] PrivKeyRequest request)
+    {
+        //request.Mnemonic = "mnemonic from your wallet"
+        //request.Index = index of the private key you would like to generate
+        
+        PrivKey privateKey = _ethereumClient.Local.GenerateAddressPrivateKey(request);
+        
+        return privateKey;
+    }
+    
+    [HttpGet(Name = "GenerateAddress")]
+    public async Task<GeneratedAddressEth> GenerateAddress(string xpub, int index)
+    {
+        //xpub = "xpub from your wallet"
+        //index = index of the address you would like to generate
+        
+        GeneratedAddressEth address = await _ethereumClient.EthereumBlockchain.EthGenerateAddressAsync(xpub, index);
+     
+        return address;
+    }
+    
+    [HttpGet(Name = "GetBalance")]
+    public async Task<EthBalance> GetBalance(string address)
+    {
+        EthBalance balance = await _ethereumClient.EthereumBlockchain.EthGetBalanceAsync(address);
+        
+        return balance;
+    }
+    
+    [HttpGet(Name = "GetTransaction")]
+    public async Task<EthTx> GetTransaction(string hash)
+    {
+        EthTx transaction = await _ethereumClient.EthereumBlockchain.EthGetTransactionAsync(hash);
+        
+        return transaction;
+    }
+    
+    [HttpPost(Name = "BlockchainTransfer")]
+    public async Task<TransactionHash> BlockchainTransfer([FromBody] TransferEthBlockchain transfer)
+    {
+        //transfer.FromPrivateKey = "private key from your wallet"
+        //transfer.To = "address you would like to send to"
+        //transfer.Amount = amount you would like to send
+        //transfer.Currency = "ETH"
+        /*
+         new TransferEthBlockchain(
+            null, 
+            0, 
+            TargetAddress, 
+            TransferEthBlockchain.CurrencyEnum.ETH, 
+            null, 
+            "0.00005", 
+            StoragePrivKey));
+        */
+        
+        TransactionHash transactionHash = await _ethereumClient.EthereumBlockchain.EthBlockchainTransferAsync(transfer);
+        
+        return transactionHash;
     }
 }
