7b04ce5ed3becc4e685f9522da0db6ea3bfc1ac0
Smrecz <diegoxlt@gmail.com>
2022-11-22 16:54:04 +0100

    Polygon added

---
+++ Tatum.CSharp.Polygon/Clients/IPolygonClient.cs
@@ -0,0 +1,32 @@
+﻿using Tatum.CSharp.Core.Api;
+using Tatum.CSharp.Core.Client;
+using Tatum.CSharp.Evm.Local;
+
+namespace Tatum.CSharp.Polygon.Clients
+{
+    public interface IPolygonClient
+    {
+        /// <summary>
+        /// Client wrapping all operations related directly to the Polygon blockchain.
+        /// </summary>
+        IPolygonApiAsync PolygonBlockchain { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related directly to the Polygon blockchain with full <see cref="ApiResponse{T}"/> return types.
+        /// </summary>
+        IPolygonApiWithHttpInfoAsync PolygonBlockchainWithHttpInfo { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related to the Polygon NFTs.
+        /// </summary>
+        INFTMaticApiAsync PolygonNft { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related to the Polygon NFTs with full <see cref="ApiResponse{T}"/> return types.
+        /// </summary>
+        INFTMaticApiWithHttpInfoAsync PolygonNftWithHttpInfo { get; }
+
+        // Service for local operations that can be used to keep all sensitive information local.
+        IEvmLocalService Local { get; }
+    }
+}
---
+++ Tatum.CSharp.Polygon/Clients/PolygonClient.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Http;
+using Tatum.CSharp.Core.Api;
+using Tatum.CSharp.Evm.Local;
+using Tatum.CSharp.Polygon.Configuration;
+
+namespace Tatum.CSharp.Polygon.Clients
+{
+    public class PolygonClient : IPolygonClient
+    {
+        /// <inheritdoc />
+        public IPolygonApiAsync PolygonBlockchain { get; }
+        
+        /// <inheritdoc />
+        public IPolygonApiWithHttpInfoAsync PolygonBlockchainWithHttpInfo { get; }
+
+        /// <inheritdoc />
+        public INFTMaticApiAsync PolygonNft { get; }
+        
+        /// <inheritdoc />
+        public INFTMaticApiWithHttpInfoAsync PolygonNftWithHttpInfo { get; }
+
+        /// <inheritdoc />
+        public IEvmLocalService Local { get; }
+        
+        /// <summary>
+        /// Creates an instance of <see cref="PolygonClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="optionsFunc">Configuration options func.</param>
+        public PolygonClient(HttpClient httpClient, Func<PolygonClientOptions, PolygonClientOptions> optionsFunc) 
+            : this(httpClient, optionsFunc(new PolygonClientOptions()))
+        {
+        }
+        
+        /// <summary>
+        /// Creates an instance of <see cref="PolygonClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="options">Configuration options.</param>
+        public PolygonClient(HttpClient httpClient, PolygonClientOptions options) 
+            : this(httpClient, options.ApiKey, options.IsTestnet)
+        {
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="PolygonClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="apiKey">Api key that will be used when calling Tatum API.</param>
+        public PolygonClient(HttpClient httpClient, string apiKey) 
+            : this(httpClient, apiKey, false)
+        {
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="PolygonClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="apiKey">Api key that will be used when calling Tatum API.</param>
+        /// <param name="isTestNet">Value indicating weather Local services should generate values for Testnet.</param>
+        public PolygonClient(HttpClient httpClient, string apiKey, bool isTestNet)
+        {
+            var polygonApi = new PolygonApi(httpClient);
+
+            polygonApi.Configuration.ApiKey.Add("x-api-key", apiKey);
+
+            PolygonBlockchain = polygonApi;
+            PolygonBlockchainWithHttpInfo = polygonApi;
+            
+            var polygonNftApi = new NFTEthApi(httpClient);
+            
+            polygonNftApi.Configuration.ApiKey.Add("x-api-key", apiKey);
+            
+            PolygonNft = polygonNftApi;
+            PolygonNftWithHttpInfo = polygonNftApi;
+
+            Local = new EvmLocalService(isTestNet);
+        }
+    }
+}
