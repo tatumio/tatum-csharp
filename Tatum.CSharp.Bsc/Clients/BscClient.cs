cce7d33f4ac91262f90a86e37a5989f36ef6bc4b
Smrecz <diegoxlt@gmail.com>
2022-11-24 12:03:34 +0100

    BSC init

---
+++ Tatum.CSharp.Bsc/Clients/BscClient.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net.Http;
+using Tatum.CSharp.Core.Api;
+using Tatum.CSharp.Bsc.Configuration;
+using Tatum.CSharp.Evm.Local;
+
+namespace Tatum.CSharp.Bsc.Clients
+{
+    public class BSCClient : IBSCClient
+    {
+        /// <inheritdoc />
+        public IBNBSmartChainApiAsync BSCBlockchain { get; }
+        
+        /// <inheritdoc />
+        public IBNBSmartChainApiWithHttpInfoAsync BSCBlockchainWithHttpInfo { get; }
+
+        /// <inheritdoc />
+        public INFTBscApiAsync BSCNft { get; }
+        
+        /// <inheritdoc />
+        public INFTBscApiWithHttpInfoAsync BSCNftWithHttpInfo { get; }
+
+        /// <inheritdoc />
+        public IEvmLocalService Local { get; }
+        
+        /// <summary>
+        /// Creates an instance of <see cref="BSCClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="optionsFunc">Configuration options func.</param>
+        public BSCClient(HttpClient httpClient, Func<BSCClientOptions, BSCClientOptions> optionsFunc) 
+            : this(httpClient, optionsFunc(new BSCClientOptions()))
+        {
+        }
+        
+        /// <summary>
+        /// Creates an instance of <see cref="BSCClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="options">Configuration options.</param>
+        public BSCClient(HttpClient httpClient, BSCClientOptions options) 
+            : this(httpClient, options.ApiKey, options.IsTestnet)
+        {
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="BSCClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="apiKey">Api key that will be used when calling Tatum API.</param>
+        public BSCClient(HttpClient httpClient, string apiKey) 
+            : this(httpClient, apiKey, false)
+        {
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="BSCClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="apiKey">Api key that will be used when calling Tatum API.</param>
+        /// <param name="isTestNet">Value indicating weather Local services should generate values for Testnet.</param>
+        public BSCClient(HttpClient httpClient, string apiKey, bool isTestNet)
+        {
+            var bscApi = new BNBSmartChainApi(httpClient);
+
+            bscApi.Configuration.ApiKey.Add("x-api-key", apiKey);
+
+            BSCBlockchain = bscApi;
+            BSCBlockchainWithHttpInfo = bscApi;
+            
+            var bscNftApi = new NFTBscApi(httpClient);
+            
+            bscNftApi.Configuration.ApiKey.Add("x-api-key", apiKey);
+            
+            BSCNft = bscNftApi;
+            BSCNftWithHttpInfo = bscNftApi;
+
+            Local = new EvmLocalService(isTestNet);
+        }
+    }
+}
---
+++ Tatum.CSharp.Bsc/Clients/IBscClient.cs
@@ -0,0 +1,32 @@
+﻿using Tatum.CSharp.Core.Api;
+using Tatum.CSharp.Core.Client;
+using Tatum.CSharp.Evm.Local;
+
+namespace Tatum.CSharp.Bsc.Clients
+{
+    public interface IBSCClient
+    {
+        /// <summary>
+        /// Client wrapping all operations related directly to the BSC blockchain.
+        /// </summary>
+        IBNBSmartChainApiAsync BSCBlockchain { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related directly to the BSC blockchain with full <see cref="ApiResponse{T}"/> return types.
+        /// </summary>
+        IBNBSmartChainApiWithHttpInfoAsync BSCBlockchainWithHttpInfo { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related to the BSC NFTs.
+        /// </summary>
+        INFTBscApiAsync BSCNft { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related to the BSC NFTs with full <see cref="ApiResponse{T}"/> return types.
+        /// </summary>
+        INFTBscApiWithHttpInfoAsync BSCNftWithHttpInfo { get; }
+
+        // Service for local operations that can be used to keep all sensitive information local.
+        IEvmLocalService Local { get; }
+    }
+}
