125303a092613cbc02785db2a09e79da0efe6fa2
Smrecz <diegoxlt@gmail.com>
2022-11-24 13:08:25 +0100

    Fix pipeline

--- Tatum.CSharp.Bsc/Clients/BscClient.cs
+++ Tatum.CSharp.Bsc/Clients/BscClient.cs
@@ -6,74 +6,74 @@ using Tatum.CSharp.Evm.Local;
 
 namespace Tatum.CSharp.Bsc.Clients
 {
-    public class BSCClient : IBSCClient
+    public class BscClient : IBscClient
     {
         /// <inheritdoc />
-        public IBNBSmartChainApiAsync BSCBlockchain { get; }
+        public IBNBSmartChainApiAsync BscBlockchain { get; }
         
         /// <inheritdoc />
-        public IBNBSmartChainApiWithHttpInfoAsync BSCBlockchainWithHttpInfo { get; }
+        public IBNBSmartChainApiWithHttpInfoAsync BscBlockchainWithHttpInfo { get; }
 
         /// <inheritdoc />
-        public INFTBscApiAsync BSCNft { get; }
+        public INFTBscApiAsync BscNft { get; }
         
         /// <inheritdoc />
-        public INFTBscApiWithHttpInfoAsync BSCNftWithHttpInfo { get; }
+        public INFTBscApiWithHttpInfoAsync BscNftWithHttpInfo { get; }
 
         /// <inheritdoc />
         public IEvmLocalService Local { get; }
         
         /// <summary>
-        /// Creates an instance of <see cref="BSCClient"/>.
+        /// Creates an instance of <see cref="BscClient"/>.
         /// </summary>
         /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
         /// <param name="optionsFunc">Configuration options func.</param>
-        public BSCClient(HttpClient httpClient, Func<BSCClientOptions, BSCClientOptions> optionsFunc) 
-            : this(httpClient, optionsFunc(new BSCClientOptions()))
+        public BscClient(HttpClient httpClient, Func<BscClientOptions, BscClientOptions> optionsFunc) 
+            : this(httpClient, optionsFunc(new BscClientOptions()))
         {
         }
         
         /// <summary>
-        /// Creates an instance of <see cref="BSCClient"/>.
+        /// Creates an instance of <see cref="BscClient"/>.
         /// </summary>
         /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
         /// <param name="options">Configuration options.</param>
-        public BSCClient(HttpClient httpClient, BSCClientOptions options) 
+        public BscClient(HttpClient httpClient, BscClientOptions options) 
             : this(httpClient, options.ApiKey, options.IsTestnet)
         {
         }
 
         /// <summary>
-        /// Creates an instance of <see cref="BSCClient"/>.
+        /// Creates an instance of <see cref="BscClient"/>.
         /// </summary>
         /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
         /// <param name="apiKey">Api key that will be used when calling Tatum API.</param>
-        public BSCClient(HttpClient httpClient, string apiKey) 
+        public BscClient(HttpClient httpClient, string apiKey) 
             : this(httpClient, apiKey, false)
         {
         }
 
         /// <summary>
-        /// Creates an instance of <see cref="BSCClient"/>.
+        /// Creates an instance of <see cref="BscClient"/>.
         /// </summary>
         /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
         /// <param name="apiKey">Api key that will be used when calling Tatum API.</param>
         /// <param name="isTestNet">Value indicating weather Local services should generate values for Testnet.</param>
-        public BSCClient(HttpClient httpClient, string apiKey, bool isTestNet)
+        public BscClient(HttpClient httpClient, string apiKey, bool isTestNet)
         {
             var bscApi = new BNBSmartChainApi(httpClient);
 
             bscApi.Configuration.ApiKey.Add("x-api-key", apiKey);
 
-            BSCBlockchain = bscApi;
-            BSCBlockchainWithHttpInfo = bscApi;
+            BscBlockchain = bscApi;
+            BscBlockchainWithHttpInfo = bscApi;
             
             var bscNftApi = new NFTBscApi(httpClient);
             
             bscNftApi.Configuration.ApiKey.Add("x-api-key", apiKey);
             
-            BSCNft = bscNftApi;
-            BSCNftWithHttpInfo = bscNftApi;
+            BscNft = bscNftApi;
+            BscNftWithHttpInfo = bscNftApi;
 
             Local = new EvmLocalService(isTestNet);
         }
