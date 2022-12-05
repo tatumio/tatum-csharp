19062862591b8bdb2d75207338effff8328a17d3
Smrecz <diegoxlt@gmail.com>
2022-12-05 13:23:26 +0100

    Test dependencies

--- Tatum.CSharp.Bitcoin.Local/BitcoinLocalService.cs
+++ Tatum.CSharp.Bitcoin.Local/BitcoinLocalService.cs
@@ -35,11 +35,6 @@ namespace Tatum.CSharp.Bitcoin.Local
             return new WalletLocal(string.Join(" ", mnemonic.Words), extPubKey);
         }
 
-        public Mnemonic GenerateMnemonic()
-        {
-            return new Mnemonic(Wordlist.English, WordCount.TwentyFour);
-        }
-
         /// <inheritdoc />
         public WalletLocal GenerateWallet(string mnemonic)
         {
--- Tatum.CSharp.Bitcoin.Local/IBitcoinLocalService.cs
+++ Tatum.CSharp.Bitcoin.Local/IBitcoinLocalService.cs
@@ -9,11 +9,6 @@ namespace Tatum.CSharp.Bitcoin.Local
         /// Generates a BIP44 compatible EVM wallet with the derivation path m/44'/60'/0'/0.
         /// </summary>
         WalletLocal GenerateWallet();
-        
-        /// <summary>
-        /// Generates a BIP44 compatible EVM wallet with the derivation path m/44'/60'/0'/0.
-        /// </summary>
-        Mnemonic GenerateMnemonic();
 
         /// <summary>
         /// Generates a BIP44 compatible EVM wallet with the derivation path m/44'/60'/0'/0.
