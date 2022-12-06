edfa36db0b6f131293d6ff628fdfc961ea14d9d1
Smrecz <diegoxlt@gmail.com>
2022-12-06 13:48:59 +0100

    Solana package

---
+++ Solana/Tatum.CSharp.Solana/Clients/ISolanaClient.cs
@@ -0,0 +1,21 @@
+﻿using Tatum.CSharp.Solana.Core.Api;
+using Tatum.CSharp.Evm.Local;
+
+namespace Tatum.CSharp.Solana.Clients
+{
+    public interface ISolanaClient
+    {
+        /// <summary>
+        /// Client wrapping all operations related directly to the Solana blockchain.
+        /// </summary>
+        ISolanaApiAsync SolanaBlockchain { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related directly to the Solana blockchain with full <see cref="ApiResponse{T}"/> return types.
+        /// </summary>
+        ISolanaApiWithHttpInfoAsync SolanaBlockchainWithHttpInfo { get; }
+
+        // Service for local operations that can be used to keep all sensitive information local.
+        ISolanaLocalService Local { get; }
+    }
+}
---
+++ Solana/Tatum.CSharp.Solana/Clients/SolanaClient.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Http;
+using Tatum.CSharp.Solana.Configuration;
+using Tatum.CSharp.Solana.Core.Api;
+using Tatum.CSharp.Evm.Local;
+
+namespace Tatum.CSharp.Solana.Clients
+{
+    public class SolanaClient : ISolanaClient
+    {
+        /// <inheritdoc />
+        public ISolanaApiAsync SolanaBlockchain { get; }
+        
+        /// <inheritdoc />
+        public ISolanaApiWithHttpInfoAsync SolanaBlockchainWithHttpInfo { get; }
+
+        /// <inheritdoc />
+        public ISolanaLocalService Local { get; }
+        
+        /// <summary>
+        /// Creates an instance of <see cref="SolanaClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="optionsFunc">Configuration options func.</param>
+        public SolanaClient(HttpClient httpClient, Func<SolanaClientOptions, SolanaClientOptions> optionsFunc) 
+            : this(httpClient, optionsFunc(new SolanaClientOptions()))
+        {
+        }
+        
+        /// <summary>
+        /// Creates an instance of <see cref="SolanaClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="options">Configuration options.</param>
+        public SolanaClient(HttpClient httpClient, SolanaClientOptions options) 
+            : this(httpClient, options.ApiKey, options.IsTestnet)
+        {
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="SolanaClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="apiKey">Api key that will be used when calling Tatum API.</param>
+        public SolanaClient(HttpClient httpClient, string apiKey) 
+            : this(httpClient, apiKey, false)
+        {
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="SolanaClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="apiKey">Api key that will be used when calling Tatum API.</param>
+        /// <param name="isTestNet">Value indicating weather Local services should generate values for Testnet.</param>
+        public SolanaClient(HttpClient httpClient, string apiKey, bool isTestNet)
+        {
+            var SolanaApi = new SolanaApi(httpClient);
+
+            SolanaApi.Configuration.ApiKey.Add("x-api-key", apiKey);
+
+            SolanaBlockchain = SolanaApi;
+            SolanaBlockchainWithHttpInfo = SolanaApi;
+
+            Local = new EvmLocalService(isTestNet);
+        }
+    }
+}
