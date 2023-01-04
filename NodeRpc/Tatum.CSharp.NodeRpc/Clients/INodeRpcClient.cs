9ca5afdf81e5cd5e7b86d12db3efaebb23e215dc
Smrecz <diegoxlt@gmail.com>
2023-01-04 11:25:59 +0100

    NodeRpc init

---
+++ NodeRpc/Tatum.CSharp.NodeRpc/Clients/INodeRpcClient.cs
@@ -0,0 +1,61 @@
+﻿using Tatum.CSharp.Evm.Local;
+using Tatum.CSharp.NodeRpc.Core.Api;
+
+namespace Tatum.CSharp.NodeRpc.Clients
+{
+    public interface INodeRpcClient
+    {
+        /// <summary>
+        /// Client wrapping all operations related to the Bitcoin NodeRpc.
+        /// </summary>
+        INodeRpcBtcApiAsync BitcoinNodeRpc { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related to the Bitcoin NodeRpc with full <see cref="ApiResponse{T}"/> return types.
+        /// </summary>
+        INodeRpcBtcApiWithHttpInfoAsync BitcoinNodeRpcWithHttpInfo { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related to the Ethereum NodeRpc.
+        /// </summary>
+        INodeRpcEthApiAsync EthereumNodeRpc { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related to the Ethereum NodeRpc with full <see cref="ApiResponse{T}"/> return types.
+        /// </summary>
+        INodeRpcEthApiWithHttpInfoAsync EthereumNodeRpcWithHttpInfo { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related to the Polygon NodeRpc.
+        /// </summary>
+        INodeRpcMaticApiAsync PolygonNodeRpc { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related to the Polygon NodeRpc with full <see cref="ApiResponse{T}"/> return types.
+        /// </summary>
+        INodeRpcMaticApiWithHttpInfoAsync PolygonNodeRpcWithHttpInfo { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related to the Bsc NodeRpc.
+        /// </summary>
+        INodeRpcBscApiAsync BscNodeRpc { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related to the Bsc NodeRpc with full <see cref="ApiResponse{T}"/> return types.
+        /// </summary>
+        INodeRpcBscApiWithHttpInfoAsync BscNodeRpcWithHttpInfo { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related to the Harmony NodeRpc.
+        /// </summary>
+        INodeRpcOneApiAsync HarmonyNodeRpc { get; }
+        
+        /// <summary>
+        /// Client wrapping all operations related to the Harmony NodeRpc with full <see cref="ApiResponse{T}"/> return types.
+        /// </summary>
+        INodeRpcOneApiWithHttpInfoAsync HarmonyNodeRpcWithHttpInfo { get; }
+
+        // Service for local operations that can be used to keep all sensitive information local.
+        IEvmLocalService Local { get; }
+    }
+}
---
+++ NodeRpc/Tatum.CSharp.NodeRpc/Clients/NodeRpcClient.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Net.Http;
+using Tatum.CSharp.Evm.Local;
+using Tatum.CSharp.NodeRpc.Configuration;
+using Tatum.CSharp.NodeRpc.Core.Api;
+
+namespace Tatum.CSharp.NodeRpc.Clients
+{
+    public class NodeRpcClient : INodeRpcClient
+    {
+        /// <inheritdoc />
+        public INodeRpcBtcApiAsync BitcoinNodeRpc { get; }
+        
+        /// <inheritdoc />
+        public INodeRpcBtcApiWithHttpInfoAsync BitcoinNodeRpcWithHttpInfo { get; }
+
+        /// <inheritdoc />
+        public INodeRpcEthApiAsync EthereumNodeRpc { get; }
+        
+        /// <inheritdoc />
+        public INodeRpcEthApiWithHttpInfoAsync EthereumNodeRpcWithHttpInfo { get; }
+        
+        /// <inheritdoc />
+        public INodeRpcMaticApiAsync PolygonNodeRpc { get; }
+        
+        /// <inheritdoc />
+        public INodeRpcMaticApiWithHttpInfoAsync PolygonNodeRpcWithHttpInfo { get; }
+        
+        /// <inheritdoc />
+        public INodeRpcBscApiAsync BscNodeRpc { get; }
+        
+        /// <inheritdoc />
+        public INodeRpcBscApiWithHttpInfoAsync BscNodeRpcWithHttpInfo { get; }
+        
+        /// <inheritdoc />
+        public INodeRpcOneApiAsync HarmonyNodeRpc { get; }
+        
+        /// <inheritdoc />
+        public INodeRpcOneApiWithHttpInfoAsync HarmonyNodeRpcWithHttpInfo { get; }
+
+        /// <inheritdoc />
+        public IEvmLocalService Local { get; }
+        
+        /// <summary>
+        /// Creates an instance of <see cref="NodeRpcClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="optionsFunc">Configuration options func.</param>
+        public NodeRpcClient(HttpClient httpClient, Func<NodeRpcClientOptions, NodeRpcClientOptions> optionsFunc) 
+            : this(httpClient, optionsFunc(new NodeRpcClientOptions()))
+        {
+        }
+        
+        /// <summary>
+        /// Creates an instance of <see cref="NodeRpcClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="options">Configuration options.</param>
+        public NodeRpcClient(HttpClient httpClient, NodeRpcClientOptions options) 
+            : this(httpClient, options.ApiKey, options.IsTestnet)
+        {
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="NodeRpcClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="apiKey">Api key that will be used when calling Tatum API.</param>
+        public NodeRpcClient(HttpClient httpClient, string apiKey) 
+            : this(httpClient, apiKey, false)
+        {
+        }
+
+        /// <summary>
+        /// Creates an instance of <see cref="NodeRpcClient"/>.
+        /// </summary>
+        /// <param name="httpClient"><see cref="HttpClient"/> Instance that should preferably be managed by HttpClient Factory.</param>
+        /// <param name="apiKey">Api key that will be used when calling Tatum API.</param>
+        /// <param name="isTestNet">Value indicating weather Local services should generate values for Testnet.</param>
+        public NodeRpcClient(HttpClient httpClient, string apiKey, bool isTestNet)
+        {
+            var ethereumNodeRpcApi = new NodeRpcEthApi(httpClient);
+            
+            ethereumNodeRpcApi.Configuration.ApiKey.Add("x-api-key", apiKey);
+            
+            EthereumNodeRpc = ethereumNodeRpcApi;
+            EthereumNodeRpcWithHttpInfo = ethereumNodeRpcApi;
+            
+            var polygonNodeRpcApi = new NodeRpcMaticApi(httpClient);
+            
+            polygonNodeRpcApi.Configuration.ApiKey.Add("x-api-key", apiKey);
+            
+            PolygonNodeRpc = polygonNodeRpcApi;
+            PolygonNodeRpcWithHttpInfo = polygonNodeRpcApi;
+            
+            var bscNodeRpcApi = new NodeRpcBscApi(httpClient);
+            
+            bscNodeRpcApi.Configuration.ApiKey.Add("x-api-key", apiKey);
+            
+            BscNodeRpc = bscNodeRpcApi;
+            BscNodeRpcWithHttpInfo = bscNodeRpcApi;
+            
+            var oneNodeRpcApi = new NodeRpcOneApi(httpClient);
+            
+            oneNodeRpcApi.Configuration.ApiKey.Add("x-api-key", apiKey);
+            
+            HarmonyNodeRpc = oneNodeRpcApi;
+            HarmonyNodeRpcWithHttpInfo = oneNodeRpcApi;
+            
+            var btcNodeRpcApi = new NodeRpcBtcApi(httpClient);
+            
+            btcNodeRpcApi.Configuration.ApiKey.Add("x-api-key", apiKey);
+            
+            BitcoinNodeRpc = btcNodeRpcApi;
+            BitcoinNodeRpcWithHttpInfo = btcNodeRpcApi;
+
+            Local = new EvmLocalService(isTestNet);
+        }
+    }
+}
