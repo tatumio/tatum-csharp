d99f3bc0956be79dbbae205e81a4e00d72793d0f
Smrecz <diegoxlt@gmail.com>
2023-01-27 10:28:45 +0100

    Cleanup

--- Unity/com.tatum.bitcoin/Bitcoin/Tatum.CSharp.Bitcoin/Configuration/BitcoinClientOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Tatum.CSharp.Bitcoin.Configuration
-{
-    public class BitcoinClientOptions
-    {
-        public bool IsTestnet { get; set; }
-        public string ApiKey { get; set; }
-    }
-}
