7a64e120d7f1801bba410a44526c5662cdc9361b
karol.recko <karol.recko@tatum.io>
2023-02-20 14:36:56 +0100

    build before executing test

--- app_hck.sh
+++ app_hck.sh
@@ -5,7 +5,7 @@ function kill_and_exit() {
     exit $1
 }
 
-dotnet run --project Tatum.CSharp.Demo > .hck_debug 2>&1 &
+dotnet run --no-build --project Tatum.CSharp.Demo > .hck_debug 2>&1 &
 
 sleep 3
 
